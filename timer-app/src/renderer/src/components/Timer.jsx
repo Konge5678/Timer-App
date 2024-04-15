dc3c29a8facb717620a85e46a0e5e6795763ffa0
KrisH <123541182+Konge5678@users.noreply.github.com>
2024-04-15 19:13:40 +0200

    Edit, Pause and stop buttons

--- timer-app/src/renderer/src/components/Timer.jsx
+++ timer-app/src/renderer/src/components/Timer.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InputField from "./inputFields";
 import { IoCheckmarkSharp, IoCaretForward, IoPencil } from "react-icons/io5";
-
+import { BsFeather } from "react-icons/bs"
+import { IoMdCloseCircleOutline, IoMdPause  } from "react-icons/io";
 export default function Timer({isOverlay}) {
     const [isEditing, setIsEditing] = useState(true);
     const [hours, setHours] = useState(0);
@@ -9,6 +10,39 @@ export default function Timer({isOverlay}) {
     const [seconds, setSeconds] = useState(0);
     const [isActive, setIsActive] = useState(false);
 
+
+    useEffect(() => {
+
+        let intervalId
+
+        if (isActive){
+            intervalId = setInterval(() => {
+                if (seconds > 0){
+                    setSeconds((seconds) => seconds - 1);
+                } else {
+                    if (minutes ===0 && hours === 0){
+
+                        clearInterval(intervalId);
+                        setIsActive(false);
+                    } else {
+                        if (minutes === 0) {
+                            setHours((hours) => hours - 1);
+                            setMinutes(59)
+                        } else {
+                            setMinutes((minutes) => minutes - 1);
+                        }
+                        setSeconds(59)
+                    }
+                }
+            }, 1000)
+        } else {
+            clearInterval(intervalId);
+        }
+        return () => clearInterval(intervalId);
+
+    }, [isActive, hours, minutes, seconds])    
+
+    
     
     return (
         <div>
@@ -30,7 +64,7 @@ export default function Timer({isOverlay}) {
                 </div>
             ):(
                 //Timer
-                <div className="flex justify-center">
+                <div className="flex justify-center items-center space-x-4">
                     <h1 className="text-pink-500 text-4xl">
                         {hours.toString().padStart(2,"0")}:
                         {minutes.toString().padStart(2,"0")}:
@@ -39,13 +73,17 @@ export default function Timer({isOverlay}) {
                         <div className="text-stone-500" id="timer-buttons">
                             {isActive ? 
                             ( <>
-                                <button>pause</button>
-                                <button>stop</button>
+                                <button className="start text-3xl text-pink-500 relative top-[3px]" onClick={()=>setIsActive(false)}><IoMdPause /></button>
+                                <button className="start text-3xl text-pink-500 relative top-[3px]" onClick={()=>{setIsActive(false) 
+                                    setHours(0) 
+                                    setMinutes(0)
+                                    setSeconds(0) }
+                                    }><IoMdCloseCircleOutline /></button>
                                 </>
                             ) : 
                             ( <>
                                 <button className="start text-4xl text-pink-500" onClick={()=>setIsActive(true)}><IoCaretForward /></button>
-                                <button className="start text-2xl text-pink-500" onClick={()=>setIsEditing(true)}><IoPencil /></button>
+                                <button className="start text-2xl text-pink-500 relative top-[-4px]" onClick={()=>setIsEditing(true)}><BsFeather /></button>
                                 </>
                             ) 
                         }
--- timer-app/src/renderer/src/components/inputFields.jsx
+++ timer-app/src/renderer/src/components/inputFields.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function InputField({label, value, onChange, placeHolder }) {
+
+    const handleInputChange = (e) => {
+        const inputValue = e.target.value;
+        if (/^\d+$/.test(inputValue)) {
+            onChange(e);
+        }
+    }
     return (
         <div className="text-2xl">
             <label className="text-stone-300">{label}:</label>
             <input type="number"
-            value={value}
-            onChange={onChange}
+            value={value} 
+            onChange={handleInputChange}
             placeholder={placeHolder}
             className="w-10 bg-transparent text-purple-800"
             />
