ca1f97ab1330bd864b6baf6efb69dd0b35ecb2fc
Kristian Haugsrud <kris.haugsrud@gmail.com>
2024-04-17 12:32:31 +0200

    simple fixes

--- timer-app/src/main/index.js
+++ timer-app/src/main/index.js
@@ -4,7 +4,7 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow() {
-  // Create the browser window.
+  // Lager et nytt vindu
   const mainWindow = new BrowserWindow({
     width: 300,
     height: 180,
@@ -19,19 +19,21 @@ function createWindow() {
     }
   })
 
-  mainWindow.setAlwaysOnTop(true, "screen");
+  // Setter tabben på toppen av alle andre vinduer
+  mainWindow.setAlwaysOnTop(true, 'screen')
 
+  // Viser appen når den er klar
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
 
+  // Håndterer når en ny nettleservindu er forespurt for å åpne
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+  // Laster inn innholdet i vinduet
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -39,51 +41,41 @@ function createWindow() {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Kjører funksjonen når appen er klar
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  ipcMain.on("close-window", () => {
-    const currentWindow = BrowserWindow.getFocusedWindow();
+  // Lytter til 'close-window' meldingen fra renderer
+  ipcMain.on('close-window', () => {
+    // Henter det fokuserte vinduet
+    const currentWindow = BrowserWindow.getFocusedWindow()
+    // Lukker det fokuserte vinduet hvis det eksisterer
     if (currentWindow) {
       currentWindow.close()
     }
   })
 
-  ipcMain.on("minimize-window", () => {
-    const currentWindow = BrowserWindow.getFocusedWindow();
+  // Lytter til 'minimize-window' meldingen fra renderer
+  ipcMain.on('minimize-window', () => {
+    // Henter det fokuserte vinduet
+    const currentWindow = BrowserWindow.getFocusedWindow()
+    // Minimerer det fokuserte vinduet hvis det eksisterer
     if (currentWindow) {
       currentWindow.minimize()
     }
   })
 
-  createWindow()
-
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
